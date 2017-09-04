c9c01120937899dbab48e57e11de1ef89ed2d09c
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-09-04 17:41:38 +0000

    Fix cooper cm-11.0 build
    Change-Id: I61f9dd183ca448de76e573b20bc91ba0e4bc7a62

--- btif/src/btif_hh.c
+++ btif/src/btif_hh.c
@@ -262,7 +262,7 @@ static BT_HDR *create_pbuf(UINT16 len)
 {
     UINT16 buflen = (UINT16) (len + BTA_HH_MIN_OFFSET + sizeof(BT_HDR));
     if (buflen < len) {
-      android_errorWriteWithInfoLog(0x534e4554, "28672558", -1, NULL, 0);
+      //android_errorWriteWithInfoLog(0x534e4554, "28672558", -1, NULL, 0);
       return NULL;
     }
     return GKI_getbuf(buflen);
